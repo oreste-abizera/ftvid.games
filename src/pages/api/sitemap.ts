beb25eeae429cef99f1f44b99b0813ca61d17280
oreste-abizera <oresteabizera11@gmail.com>
2021-06-10 21:01:46 +0200

    Adding all video links to sitemap

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -1,9 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import url from '@/utils/url';
 
 const { SitemapStream, streamToPromise } = require('sitemap');
 const { Readable } = require('stream');
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let videosIds: never[] = [];
+  let response = await axios
+    .get(`${url}/indexing`)
+    .catch((err) => console.log('error occured'));
+  if (response) {
+    videosIds = response.data.matches || [];
+  }
   // An array with your links
   const links = [
     { url: '/', changefreq: 'daily', priority: 0.3 },
@@ -11,6 +20,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     { url: '/search', changefreq: 'daily', priority: 0.3 },
     { url: '/videos/[id]', changefreq: 'daily', priority: 0.3 },
   ];
+
+  for (const id of videosIds) {
+    links.push({ url: `/videos/${id}`, changefreq: 'daily', priority: 0.3 });
+  }
 
   // Create a stream to write to
   const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
